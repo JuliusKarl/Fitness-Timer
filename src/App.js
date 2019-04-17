1f4a0cc64f8c4381fdfad5edceceb7cce0613673
Julius Macrohon <julius.karl.macrohon@gmail.com>
2019-04-18 04:41:28 +1200

    added loading screen. reduced audio size

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import ReactDom from 'react-dom'
 import BeepTest from './Components/BeepTest'
 import Interval from './Components/Interval'
 import Random from './Components/Random'
+import Spinner from 'react-spinner-material';
 
 import beep from './resources/beep.mp3'
 import beepTestAudio from './resources/beeptest.mp3'
@@ -23,15 +24,23 @@ class App extends Component {
       Interval: false,
       Random: false,
       BeepTest: false,
+      loaded: false
     }
     this.handleChange = this.handleChange.bind(this)
     this.beep = new Audio(beep);
     this.beepTestAudio = new Audio(beepTestAudio);
+    this.beep.load()
+    this.beepTestAudio.load()
   }
 
   componentDidMount() {
-    this.beep.load()
-    this.beepTestAudio.load()
+    this.beep.oncanplaythrough = () => {
+      this.beepTestAudio.oncanplaythrough = () => {
+        this.setState({
+          loaded: true
+        })
+      }
+    }
   }
 
   handleChange(event) {
@@ -46,37 +55,43 @@ class App extends Component {
   }
   render() {
     return (
-      <div className="main">
-          <button
-            name="home"
-            className="headerIcon"
-            onClick={this.handleChange}>
-              Fitness Trainer
-          </button>
-          {this.state.home ? 
-          <div className="activeWindow">
-            <button 
-              name="Interval" 
-              className="btn" 
+      <div>
+        <div className="activeWindow" style={{display: this.state.loaded && "none"}}>
+              <Spinner size={100} spinnerColor={"#333"} spinnerWidth={6} visible={true} />
+              <h2>Loading...</h2>
+        </div>
+        <div className="main" style={{display: !this.state.loaded && "none"}}>
+            <button
+              name="home"
+              className="headerIcon"
               onClick={this.handleChange}>
-                Interval Beeper
+                Fitness Trainer
             </button>
-            <button 
-              name="Random" 
-              className="btn" 
-              onClick={this.handleChange}>
-                Random Beeper
-            </button>
-            <button 
-              name="BeepTest" 
-              className="btn" 
-              onClick={this.handleChange}>
-                Beep Test
-            </button>
-          </div> : <p></p>}
-          {this.state.Interval? <Interval audio={this.beep} exists={this.state.Interval}/>: <p></p>}
-          {this.state.Random? <Random audio={this.beep} exists={this.state.Random}/>: <p></p>}
-          {this.state.BeepTest? <BeepTest audio={this.beepTestAudio} exists={this.state.BeepTest}/>: <p></p>}
+            {this.state.home ? 
+            <div className="activeWindow">
+              <button 
+                name="Interval" 
+                className="btn" 
+                onClick={this.handleChange}>
+                  Interval Beeper
+              </button>
+              <button 
+                name="Random" 
+                className="btn" 
+                onClick={this.handleChange}>
+                  Random Beeper
+              </button>
+              <button 
+                name="BeepTest" 
+                className="btn" 
+                onClick={this.handleChange}>
+                  Beep Test
+              </button>
+            </div> : <p></p>}
+            {this.state.Interval? <Interval audio={this.beep} exists={this.state.Interval}/>: <p></p>}
+            {this.state.Random? <Random audio={this.beep} exists={this.state.Random}/>: <p></p>}
+            {this.state.BeepTest? <BeepTest audio={this.beepTestAudio} exists={this.state.BeepTest}/>: <p></p>}
+        </div>
       </div>
     );
   }
